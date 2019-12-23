23ee4d850c907fa23135a7609559ea01fcc99e77
Pedram <tadayoni@gmail.com>
2019-12-22 23:06:17 -0800

    Added authentication and authorization

--- src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -2,10 +2,20 @@ package com.example.demo.model.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.Column;
+
 public class CreateUserRequest {
 
 	@JsonProperty
 	private String username;
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Column(nullable = false)
+	private String password;
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Column(nullable = false)
+	private String confirmPassword;
 
 	public String getUsername() {
 		return username;
@@ -13,5 +23,21 @@ public class CreateUserRequest {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
 	}
 }
